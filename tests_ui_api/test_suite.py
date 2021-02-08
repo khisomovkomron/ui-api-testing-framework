ff707182ac8551d85be84d9add8480d2e9587e7f
khisomovkomron <normok_9595@bk.ru>
2021-02-08 23:50:53 +0300

    changed test suite

--- tests_ui_api/test_suite.py
+++ tests_ui_api/test_suite.py
@@ -2,13 +2,13 @@ import unittest
 from tests_ui_api.ui_tests.test_login import TestLogin
 from HtmlTestRunner import HTMLTestRunner
 tc1 = unittest.TestLoader().loadTestsFromTestCase(TestLogin) #smoke test for example
-# tc2 =unittest.TestLoader().loadTestsFromTestCase(TestSignup) #functional test for example
+tc2 =unittest.TestLoader().loadTestsFromTestCase(TestSignup) #functional test for example
 # tc3 =unittest.TestLoader().loadTestsFromTestCase(TestHome) #smoke test for example
 
 smokeTest = unittest.TestSuite([tc1])
-# functionalTest =unittest.TestSuite([tc2])
-# smokeTest =unittest.TestSuite([tc3])
-# regressionTest =unittest.TestSuite([tc1, tc2, tc3])
+functionalTest =unittest.TestSuite([tc2])
+smokeTest =unittest.TestSuite([tc3])
+regressionTest =unittest.TestSuite([tc1, tc2, tc3])
 
 
 
@@ -23,6 +23,6 @@ runner = HTMLTestRunner(
                 )
 
 runner.run(tc1)
-# runner.run(tc2)
-# runner.run(tc3)
-# runner.run(tc4)
+runner.run(tc2)
+runner.run(tc3)
+runner.run(tc4)
